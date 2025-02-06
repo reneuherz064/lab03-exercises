09cf56c5a13671f99d663b5b3d1a806d795da281
Ryan <rneuherz@unca.edu>
2025-02-05 20:06:14 -0500

    add find_duplicates to the repository

--- python/find_duplicates.py
+++ python/find_duplicates.py
@@ -17,3 +17,21 @@ if __name__ == "__main__":
     print("Sample 2:", find_duplicates_nested_loop(sample2))
     print("Sample 3:", find_duplicates_nested_loop(sample3))
     print("Sample 4:", find_duplicates_nested_loop(sample4))
+
+# Was I supposed to use a set or a dictionary? The instructions had links for set
+# documentation but it said to use a dictionary under that line of instruction. Also
+# I did not know how to do this with a dictionary or set.
+def find_duplicates_sets(s: set) -> set:
+    duplicates = set(s)
+    return duplicates
+
+if __name__ == "__main__":
+    sample1 = {3, 7, 5, 6, 7, 4, 8, 5, 7, 66}
+    sample2 = {3, 5, 6, 4, 4, 5, 66, 6, 7, 6}
+    sample3 = {3, 0, 5, 1, 0}
+    sample4 = {3}
+    
+    print("Sample 1:", find_duplicates_sets(sample1))
+    print("Sample 2:", find_duplicates_sets(sample2))
+    print("Sample 3:", find_duplicates_sets(sample3))
+    print("Sample 4:", find_duplicates_sets(sample4))
